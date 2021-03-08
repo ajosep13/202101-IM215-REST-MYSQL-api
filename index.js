71c6c61f4d1d706340f23cb9945a16eccdd435de
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-03-08 10:13:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-console.log("Hi");
-person = "Ammu";
-character = {
-    name:person,
-    age:'24',
-}
-console.log(person);
-console.log(character);
+const express = require('express');
+const app = express();
+app.get('/', (request,response) => {
+console.log('Request Received');
+//  response.send('Welcome');
+response.json({msg : 'Welcome'});
+});
+app.listen(3333, () =>{
+    console.log('The server is up and listening on port 3333')
+});
