4a3ba175057d4dad61598263f96c3e4ad0cd4e1d
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-03-15 09:52:49 -0400

    morgan, nodemon and query param

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
+const { response } = require('express');
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:false}))
 app.get('/', (request,response) => {
-console.log('Request Received');
+// console.log('Request Received');
 //  response.send('Welcome');
 //response.json({msg : 'Welcome'});
+/**
+ * Types of parameters:
+ * Query param1=val 1&param2=val2
+ * Route localhost:3333/name/val1/age/val2/birthday/val3....
+ * Body param1=val 1&param2=val2 (if using form)
+ */
+//Query
+// console.log('Query Parameters');
+// console.log(request.query);
+
+//Body
+// console.log('Body Parameters');
+// console.log(request.body);
 response.end();
 });
-app.listen(3333, () =>{
+app.get('/:first_name/:last_name/:age',(request, response) => {
+  //Route
+console.log('Query Parameters');
+console.log(request.params);
+response.end();  
+})
+app.get('/id/:user_id',(request, response) => {
+  //Route
+console.log('Route Parameter');
+console.log(request.params);
+console.log('Body Parameters');
+console.log(request.body);
+console.log('Query Parameters');
+console.log(request.query);
+response.end();  
+})
+app.listen(3333, () => {
     console.log('The server is up and listening on port 3333')
 });
